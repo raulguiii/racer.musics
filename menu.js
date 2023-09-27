ba444196b3ff571de89005e0ca25f996d2bfe63c
Raul Guilherme <143142847+raulguiii@users.noreply.github.com>
2023-09-27 17:16:54 -0300

    menu.js

--- menu.js
+++ menu.js
@@ -26,7 +26,7 @@ document.getElementById("botaoDeBuscaSidebar").addEventListener("click", functio
     document.getElementById("barraDePesquisa").value = termoDePesquisaSidebar;
 });
 
-        // Player de música \\
+        // Player da música 1\\
         const audioPlayer1 = document.getElementById('audio-player1');
         const playButton1 = document.getElementById('play-button1');
 
@@ -40,8 +40,11 @@ document.getElementById("botaoDeBuscaSidebar").addEventListener("click", functio
             }
         });
 
-        const audioPlayer2 = document.getElementById('audio-player5');
-        const playButton2 = document.getElementById('play-button5');
+
+
+        // Player da música 2\\
+        const audioPlayer2 = document.getElementById('audio-player2');
+        const playButton2 = document.getElementById('play-button2');
 
         playButton2.addEventListener('click', () => {
             if (audioPlayer2.paused) {
@@ -50,5 +53,69 @@ document.getElementById("botaoDeBuscaSidebar").addEventListener("click", functio
             } else {
                 audioPlayer2.pause();
                 playButton2.innerHTML = '<span class="fa fa-play"></span>';
+            }
+        });
+
+
+
+        //Player da música 3\\
+        const audioPlayer3 = document.getElementById('audio-player3');
+        const playButton3 = document.getElementById('play-button3');
+
+        playButton3.addEventListener('click', () => {
+            if (audioPlayer3.paused) {
+                audioPlayer3.play();
+                playButton3.innerHTML = '<span class="fa fa-pause"></span>';
+            } else {
+                audioPlayer3.pause();
+                playButton3.innerHTML = '<span class="fa fa-play"></span>';
+            }
+        });
+
+
+
+        //Player da música 4\\
+        const audioPlayer4 = document.getElementById('audio-player4');
+        const playButton4 = document.getElementById('play-button4');
+
+        playButton4.addEventListener('click', () => {
+            if (audioPlayer4.paused) {
+                audioPlayer4.play();
+                playButton4.innerHTML = '<span class="fa fa-pause"></span>';
+            } else {
+                audioPlayer4.pause();
+                playButton4.innerHTML = '<span class="fa fa-play"></span>';
+            }
+        });
+
+
+
+        //Player da música 5\\
+        const audioPlayer5 = document.getElementById('audio-player5');
+        const playButton5 = document.getElementById('play-button5');
+
+        playButton5.addEventListener('click', () => {
+            if (audioPlayer5.paused) {
+                audioPlayer5.play();
+                playButton5.innerHTML = '<span class="fa fa-pause"></span>';
+            } else {
+                audioPlayer5.pause();
+                playButton5.innerHTML = '<span class="fa fa-play"></span>';
+            }
+        });
+
+
+
+        //Player da música 6\\
+        const audioPlayer6 = document.getElementById('audio-player6');
+        const playButton6 = document.getElementById('play-button6');
+
+        playButton6.addEventListener('click', () => {
+            if (audioPlayer6.paused) {
+                audioPlayer6.play();
+                playButton6.innerHTML = '<span class="fa fa-pause"></span>';
+            } else {
+                audioPlayer6.pause();
+                playButton6.innerHTML = '<span class="fa fa-play"></span>';
             }
         });
