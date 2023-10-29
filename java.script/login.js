e2dabb1f663392e259b40664b313f146a68a15a4
Raul Guilherme <143142847+raulguiii@users.noreply.github.com>
2023-10-29 18:12:53 -0300

    login.js

--- java.script/login.js
+++ java.script/login.js
@@ -1,17 +1,33 @@
 document.getElementById("form").addEventListener("submit", function(event) {
-    // Impedir o envio do formulário em branco
+    
+    
+    // garantir que o form não seja respondido vazio 
     event.preventDefault();
 
-    // Obter os valores dos campos de entrada
+    // receber os valores do form 
     var usuario = document.getElementById("usuario").value;
     var senha = document.getElementById("senha").value;
 
-    // Realizar a validação (verificar se os campos estão preenchidos)
-    if (usuario === "" || senha === "") {
-        alert("Por favor, preencha todos os campos.");
-    } else {
-        // Se a validação for bem-sucedida, você pode enviar o formulário ou executar outra ação aqui
-    window.location = "http://127.0.0.1:5500/racermenu.html";
-        // Você pode adicionar código para enviar o formulário para o servidor aqui
-    }
-});
+    // ver se a conta é valida e existe
+        if (usuario === "raulguiii" && senha === "senha123") {
+          window.location = "file:///C:/Users/rauln/OneDrive/%C3%81rea%20de%20Trabalho/projeto%20desen/racermenu.html";
+        } else {
+            alert("Essa conta não existe!");
+        }
+
+    })
+
+
+
+
+
+
+
+
+    // Versão antiga, entrada sem acesso 
+//    if (usuario === "" || senha === "") {
+//        alert("Por favor, preencha todos os campos.");
+//    } else {
+//    window.location = "http://127.0.0.1:5500/racermenu.html";
+//    }
+// });
